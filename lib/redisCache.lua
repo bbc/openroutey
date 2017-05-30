f6b1e14e090dc4e2668fb7bf81e240cfa20fa89a
Matthew Clark <matthew1000@gmail.com>
2017-05-30 08:12:51 +0100

    Tidier code, better json handling

--- lib/redisCache.lua
+++ lib/redisCache.lua
@@ -1,38 +1,37 @@
 local M = {} -- public interface
 local redisLib = require "resty.redis"
-local JSON = require "cjson"
-local config = require "config"
 
 -- Some constants. We use strings as values to make it easier to debug.
 M.HIT = 'HIT'
 M.MISS = 'MISS'
 M.STALE = 'STALE'
 
--- Given a URL and response from origin, stores it in Redis
+-- Given a URL and response from origin, stores it in Redis, if permitted
 function M.storeResponse(url, response)
     local cacheSeconds = getCacheSeconds(response)
-    -- ngx.log(ngx.ERR, "cache seconds for: ", url, " is ", cacheSeconds, " and etag is ", response.header["Etag"])
-    -- ngx.log(ngx.ERR, "HEADER:", JSON.encode(response.header))
-    if cacheSeconds == nil then return end
+    if cacheSeconds == nil then return end -- Don't cache if we're not supposed to
+
+    local responseEncoded, responseErrors = jsonSafe.encode(response)
+    if (reponseErrors) then
+        ngx.log(ngx.ERR, "Failed to encode response:" .. reponseErrors)
+        return
+    end
 
     local properties = {
-        url= url, timestamp= ngx.now(),
-        cacheSeconds=cacheSeconds, response= JSON.encode(response)
+        url = url,
+        timestamp = ngx.now(),
+        cacheSeconds = cacheSeconds,
+        response = responseEncoded
     }
 
     if (response.header["ETag"]) then properties.etag = response.header["ETag"] end
-    if (response.header["Etag"]) then properties.etag = response.header["Etag"] end
 
     local redis = getRedis()
     if not redis then return end
     ok, err = redis:hmset("origin:" .. url, properties)
     closeRedis(redis)
 
-    if not ok then
-        ngx.log(ngx.ERR, "Failed to set origin in Redis:" .. err)
-        return value
-    end
-    return
+    if not ok then ngx.log(ngx.ERR, "Failed to set origin in Redis:" .. err) end
 end
 
 -- Returns:
@@ -43,7 +42,6 @@ function M.urlStatus(url)
     local redis = getRedis()
     if (not redis) then return M.MISS end
     local res,err = redis:hmget("origin:" .. url, "timestamp", "cacheSeconds", "etag")
-    -- ngx.log('REDIS ERROR:', ngx.ERR, "urlStatus:", url, " timestamp ", res[1], "cachSec:", res[2])
     if (err or not res) then
         ngx.log(ngx.ERR, 'REDIS ERROR:', err)
         return M.MISS
@@ -55,14 +53,16 @@ function M.urlStatus(url)
     if timestamp == nil or cacheSeconds == nil then return M.MISS end
 
     local isFresh = (timestamp + cacheSeconds > ngx.now())
-    -- ngx.log(ngx.ERR, "urlStatus:", url, " timestamp ", res[1], "cachSec:", res[2], "DIFF", (timestamp + cacheSeconds) - ngx.now())
     if isFresh then return M.HIT else return M.STALE,etag end
 end
 
+-- Given a URL, returns the cache of the respone we received from origin
 function M.getCachedResponse(url)
     local redis = getRedis()
     local responseJson,err = redis:hget("origin:" .. url, "response")
-    return JSON.decode(responseJson)
+    local responseDecoded, responseErrors = jsonSafe.decode(responseJson)
+    if responseErrors then ngx.log(ngx.ERR, "Failed to set decode:" .. err) end
+    return responseDecoded
 end
 
 -- Given a response, returns the number of seconds it can be cached for, or nil if not
@@ -78,7 +78,11 @@ end
 
 function getRedis()
     local redis = redisLib:new()
-    redis:set_timeout(1000) -- 1 sec
+
+    -- 1 second timeout. Intentionally small so as not to offer very slow responses
+    -- if Redis is struggling.
+    redis:set_timeout(1000)
+
     local ok, err = redis:connect(config.redisHost, config.redisPort)
     if not ok then
         ngx.log(ngx.ERR, "failed to connect to Redis: ", err)
